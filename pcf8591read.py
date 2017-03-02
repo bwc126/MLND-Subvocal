fdd27950f6e945692b5e623ab3e5e3e9c8682d40
Brian Coe <bwc126@gmail.com>
2017-03-02 13:48:49 -0500

    feat: Change file ext, use key capture loop

--- pcf8591read.py
+++ pcf8591read.py
@@ -16,7 +16,7 @@ root = Tk()
 subvocal = ''
 
 def key(event):
-  if event.char == 's':
+  if event.char == 'a':
     subvocal = 'Y'
     voltage = pin1.value * 3.3
     print("read: {} : {}".format(count, voltage))
@@ -24,6 +24,10 @@ def key(event):
     write_file('test', current, voltage, subvocal)
   else:
     subvocal = "N"
+    voltage = pin1.value * 3.3
+    print("read: {} : {}".format(count, voltage))
+    current = time.clock() - start
+    write_file('test', current, voltage, subvocal)
 
 with I2CMaster() as i2c:
     adc = PCF8591(i2c, THREE_DIFFERENTIAL)
@@ -40,13 +44,13 @@ with I2CMaster() as i2c:
     root.mainloop()
 
 
-    while True:
-        voltage = pin1.value * 3.3
-        print("read: {} : {}".format(count, voltage))
-        #sleep(0.1)
-        count += 1
-        current = time.clock() - start
-
-        write_file('test', current, voltage, )
-        #data[count] = pin1.raw_value
-        #volt_plot(count, data)
+    # while True:
+    #     voltage = pin1.value * 3.3
+    #     print("read: {} : {}".format(count, voltage))
+    #     #sleep(0.1)
+    #     count += 1
+    #     current = time.clock() - start
+    #
+    #     write_file('test', current, voltage, )
+    #     #data[count] = pin1.raw_value
+    #     #volt_plot(count, data)
