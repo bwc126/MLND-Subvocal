c154f3b86f34271e1fe657bf0564fd05fcae515b
Brian Coe <bwc126@gmail.com>
2017-05-02 10:55:55 -0400

    feat: Continue stubbing EMG_preparer, class method syntax fix

--- prepare_EMG.py
+++ prepare_EMG.py
@@ -7,7 +7,20 @@ class EMG_preparer():
     Attributes:
         window_size: float. Specifies size, in milliseconds, of the Fourier transformed windows to return.
         real_time: boolean. If True, EMG data has real units of time (such as ns or ms). If False, EMG data has sequential sample number as temporal indicator (dimensionless count).
-        sample_rate: float. Specifies number of samples per second. Only needed if real_time = False.
-        power_spectrum: boolean. Specifies whether to find a power spectrum using the data after FT has been applied. Practically, this means finding the absolute values of the intensities returned by the Fourier transform so all results are positive.
+        sample_rate: float. Specifies number of samples per second.
+        power_spectrum: boolean. Specifies whether to find a power spectrum using the data after FT has been applied. Practically, this means finding the absolute values of the intensities returned by the Fourier transform so all results are positive. If false, the raw rfft results are used for each window.
 
     """
+    def __init__(self, window_size=50.0, real_time=False, sample_rate=1000, power_spectrum=True):
+        """ Initialize the EMG_preparer Object. """
+        self.window_size = window_size
+        self.samples_per_window = sample_rate / 1000.0 * self.window_size
+    def process(self, data):
+        """ process is a method for processing data into Fourier transformed windows
+
+        Attributes:
+            data: a pandas DataFrame containing subvocalization EMG data
+        Returns:
+            a DataFrame containing FFT processed windows derived from the EMG data.
+        """
+        pass
--- prepare_outputs.py
+++ prepare_outputs.py
@@ -6,25 +6,19 @@ class output_preparer():
     """ Prepares the target data labels. Takes text, transforms it into phonemes, and then decomposes each phoneme into an array of phonological features. These arrays are returned for association with EMG data.
 
     Attributes:
-
-
-
+        subvocal_detector: Optional. An estimator trained to detect subvocalizations in EMG windows. This estimator simply returns 'True' or 'False' for whether an EMG window it's passed contains subvocalization. This is only used with the 'zip' method for the output_preparer class when that method's 'auto_align' attribute is True.
     """
-    def __init__(subvocal_detector=None):
+    def __init__(self, subvocal_detector=None):
         """ Initializes the output_preparer class.
-
-        Attributes:
-            subvocal_detector: Optional. An estimator trained to detect subvocalizations in EMG windows. This estimator simply returns 'True' or 'False' for whether an EMG window it's passed contains subvocalization. This is only used with the 'zip' method for the output_preparer class when that method's 'auto_align' attribute is True.
-
         """
         pass
 
-    def transform(text):
+    def transform(self, text):
         """ Transforms 'text', a string, into arrays of phonological features corresponding to phonemes. Returns a DataFrame of phological features and their corresponding phonemes.
         """
         pass
 
-    def zip(data, labels, auto_align=True):
+    def zip(self, data, labels, auto_align=True):
         """ Zips data and labels such that labels are sequentially applied to serial rows of 'data' that most likely contain subvocalizations. If the data is already boolean labeled for containing subvocalization, 'auto_align' should be false to make use of those labels.
 
         Attributes:
