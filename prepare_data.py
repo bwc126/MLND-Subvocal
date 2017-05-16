83fed17149a8b65491b3f418a9f147e3ffe46e9d
Brian Coe <bwc126@gmail.com>
2017-05-16 19:47:40 -0400

    feat: Use 'pandas.read_csv' method to create dataframe

--- prepare_data.py
+++ prepare_data.py
@@ -1,5 +1,13 @@
 # TODO: Load the csv files into dataframes here.
+import csv
+import pandas
 
 class data_preparer():
-    def __init__():
-    
+    def __init__(self):
+        pass
+    def load(self, filename):
+        # X = pandas.DataFrame()
+        # with open(filename) as csvfile:
+        #     filereader = csv.reader(csvfile,delimiter=',')
+        X = pandas.read_csv(filename)
+        return X
