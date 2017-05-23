b8214e3d8818ec70dd9c0f30765aa034bf4ebee5
Brian Coe <bwc126@gmail.com>
2017-05-22 20:03:11 -0400

    fix: Simplify dataframe building

--- prepare_data.py
+++ prepare_data.py
@@ -18,8 +18,8 @@ class data_preparer():
         data_4 = pandas.read_csv('Sat Mar  4 00:47:01 2017')
         data_5 = pandas.read_csv('Sat Mar  4 00:47:36 2017')
         # Select a few portions of the above datafiles that most likely contain little or no specific subvocalization, SV.
-        no_sv = data_1[:3000].append(data_2[:2000]).append(data_3[:2000]).append(data_4[:2000]).append(data_5[:2000])
+        no_sv = data_1[:3000].append([data_2[:2000],data_3[:2000],data_4[:2000],data_5[:2000]],ignore_index=True)
         # Now select a few portions that most likely do contain some specific SV.
-        with_sv = data_1[5000:9000].append(data_2[6000:10000]).append(data_3[3000:9000])
-        
+        with_sv = data_1[5000:9000].append([data_2[6000:10000],data_3[3000:9000]],ignore_index=True)
+
         return no_sv, with_sv
