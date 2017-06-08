841f5a02e879b133b48bbdafe95f418b9c091ac9
Brian Coe <bwc126@gmail.com>
2017-06-08 13:51:27 -0400

    feat: Improve SVR recording interface
    Use single word-per-file approach, with single-button start/stop recording. This should make labeling the data accurately much easier, as each file will contain only a single, known word, with very little leading and ending noise

---
+++ svr_record.py
@@ -0,0 +1,46 @@
+from tkinter import *
+from pcf8591read import *
+
+root = Tk()
+
+def testytesty():
+    print ('oh look we used a callback')
+
+words = ['dusty','march','direful','complete','superb','poised','wait','quaint','save','copy','interest','separate','bright','utter','bored','nondescript','license','vest','dance','money','languid','swim','enthusiastic','quartz','planes','spiritual','imperfect','coal','hobbies','sound','bow','squirrel','push','treatment','mine','precede','weather','amazing','round','stingy','signal','marry','country','uncle','dust','certain','loose','knock','advice','confuse','animated','loving','feeling','absorbing','trick','spare','rod','caption','raspy','throne','clumsy','vague','tow','hang','rely','tired','barbarous','pan','innocent','combative','low','rub','mixed','actually','faulty','thirsty','dam','doubtful','flowers','defective','frogs','outstanding','ducks','icicle','fry','load','cracker','efficient','hop','fax','fancy','reading','real','addicted','motion','clean','unsuitable','race','aspiring','gold','check','bouncy','regret','chop','various','eminent','wander','living','equable','cluttered','geese','tightfisted','aftermath','quince','division','board','amuck','pretty','extra',-large'sun','person','magical','invent','flap','stomach','black','river','town','type','stereotyped','paddle','expand','puncture','cakes','measly','kitty','courageous','shoe','number','third','ugliest','haircut','increase','wrathful','jog','straw','whisper','kick','talented','curious']
+
+current = 0
+
+reader = adc_reader()
+
+def key(event):
+    print ("pressed", repr(event.char))
+    if event.char.isdigit():
+        suffix = event.char
+        print ('file series will take',suffix,'as suffix in filename')
+    if event.char==' ':
+        if reader.record == False:
+            # Get the next word
+            current_word = words[current]
+            # Start the recording for that word
+            print(current_word)
+            filename = current_word + suffix
+            reader.run(filename)
+
+        # If the recording is running:
+        if reader.record:
+            reader.record = False
+            # Stop the recording
+            current += 1
+            # Iterate the word
+
+
+def callback(event):
+    frame.focus_set()
+    print ("clicked at", event.x, event.y)
+
+frame = Frame(root, width=100, height=100)
+frame.bind("<Key>", key)
+frame.bind("<Button-1>", callback)
+frame.pack()
+
+root.mainloop()
