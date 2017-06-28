6a0d1dd6d96515af99054529e583115a25e604f7
Brian Coe <bwc126@gmail.com>
2017-06-28 14:10:15 -0400

    docs: Describe simple_svr helper script

--- simple_svr.py
+++ simple_svr.py
@@ -1,5 +1,7 @@
 import pcf8591read
-
+"""
+This is a barebones script for controlling the work flow of recording EMG words and associating the data with a specific word, captured in the filename.
+"""
 reader = pcf8591read.adc_reader()
 filename = input('Current word:')
 reader.record = True
